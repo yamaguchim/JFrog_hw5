ad9a3297dbdcd1b9eb733a86bac91c755e3840b6
yamaguchim <maya9042@colorado.edu>
2017-05-08 02:16:58 -0700

    project upload

---
+++ src/main/java/com/mariyamaguchi/app/App.java
@@ -0,0 +1,13 @@
+package com.mariyamaguchi.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Release Fast Or Die!" );
+    }
+}
